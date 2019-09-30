fea19e14e5a9a5f664b60700a9bc34dac6a50c79
smilephoenix103 <marugotomoaki@gmail.com>
2019-09-30 13:32:52 -0500

    RR

---
+++ VagrantBox/Ruby/Fundamentals/Arrays.rb
@@ -0,0 +1,15 @@
+arr1 = ["Kevin", 1, "Camp", false, 3]
+arr2 = [5,1,2,6,4]
+
+
+arr1.at(2)
+arr1.fetch(2)
+arr1.delete(3)
+arr1.reverse
+arr1.length
+arr2.sort
+arr1.slice(2..3)
+arr1.shuffle
+arr1.join(" and ")
+arr2.insert(1, 2)
+puts arr1.values_at(0,2)
